83715fe49bede9399874f4a8f58527bff7d389ca
MUSHABE <mushabe.martin@student.moringaschool.com>
2023-09-28 14:39:35 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  }
+  
+  function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+  }
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  function createFareMultiplier(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    }
+  }
+  
+  function fareDoubler(fare) {
+    return createFareMultiplier(2)(fare);
+  }
+  
+  function fareTripler(fare) {
+    return createFareMultiplier(3)(fare);
+  }
+  
+  function selectDifferentDrivers(drivers, fn) {
+    return fn(drivers);
+  }
+  
+  module.exports = {
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers,
+    selectingDrivers,
+    createFareMultiplier,
+    fareDoubler,
+    fareTripler,
+    selectDifferentDrivers
+  };
